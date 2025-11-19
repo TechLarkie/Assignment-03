df5da102dcf99657e79f83e81b8796a172dcda15
TechLarkie <zeiad.alarkan@ontariotechu.net>
2025-11-18 19:26:31 -0500

    changed the port from 3000 to 4000 for developmental purposes and implemented a basic model for the music and what kind of data will be collected, in this case the data that will be collected is the name of the song, the artist who wrote the song and the genre which the song can be catagorized under

---
+++ models/music.js
@@ -0,0 +1,23 @@
+// creating code which will allow the site user to save music
+
+
+
+// creating the 'music' model, which serves as a template for what is actually stored when a user types in the songs which they like
+
+
+let musicModel = mongoose.Schema(
+    {
+        name: String,
+        Musician: String,
+        Genre: String,
+        //saving basic information about the song such as the name, the musician who wrote the song and which genre it belongs to
+
+    },
+
+    {
+        collection:"music"
+    }
+
+);
+
+module.exports=mongoose.model("music",musicModel);
